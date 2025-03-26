cb1dce2de9cab8ad23570423f805c97bcc453374
shebin ks <shebinks01@gmail.com>
2025-03-26 10:13:48 +0530

    implemented cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors'
 import { adminAuthRoutes, studentRoutes, uploadRoutes } from './routes/index.js';
 
 import path from 'path';
@@ -18,6 +19,7 @@ ensureUploadDirectory;
 dotenv.config();
 
 const app = express();
+app.use(cors());
 
 
 app.use(express.json());
